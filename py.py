a30fe024fbaa045304bb562a23a05928fbc9b431
radhold <fomenko.yaroslav@gmail.com>
2021-09-04 20:14:36 +0300

    mkdir

--- py.py
+++ py.py
@@ -1,8 +1,18 @@
 import sys
+import requests
+from six.moves import http_client
 
 
-def mkdir():
-    pass
+def mkdir(url, directoryName, path):
+    for i in path:
+        url += f"{i}/"
+    url += f"{directoryName}?\\user.name={user}&op=MKDIRS"
+    print(url)
+    response = requests.Session().put(url)
+    if response.status_code == http_client.OK:
+        print("Создание прошло успешно")
+    else:
+        print("Произошла ошибка при создании")
 
 
 def put():
@@ -52,4 +62,33 @@ if __name__ == "__main__":
     else:
         print("Ошибка. Неверное количество параметров.")
         sys.exit(1)
-defName = input("Введите название функции")
+request = requests.Session()
+urlPattern = f"http://{host}:{port}/webhdfs/v1/"
+while True:
+    defName = input("Введите название функции: ").lower()
+    if defName == "mkdir":
+        directoryName = input("Введите название директории, которую хотите создать: ")
+        path = input("Введите путь к директории без / через пробел: ").split(" ")
+        mkdir(urlPattern, directoryName, path)
+    elif defName == "put":
+        pass
+    elif defName == "get":
+        pass
+    elif defName == "append":
+        pass
+    elif defName == "delete":
+        pass
+    elif defName == "ls":
+        pass
+    elif defName == "cd":
+        pass
+    elif defName == "lls":
+        pass
+    elif defName == "lcd":
+        pass
+    elif defName == "exit":
+        print("Выход")
+        sys.exit(1)
+    else:
+        print("Некорректная функция.")
+
